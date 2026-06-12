91f06d95a71ee7e4a14a6481623f218fc4bcc636
Aarón Santiago <tadeoso0150@gmail.com>
2026-06-11 20:41:17 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.$docsify = {
   name: 'AWS re/Start',
   repo: 'https://github.com/Aaron-Santiago/Laboratorios-AWS', 
-  loadSidebar: true, 
+  loadSidebar: false, 
   homepage: 'README.md'
 }
